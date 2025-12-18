5f75a4b534ad3513ba2b0fd6b4caeadb565c780b
ehabmalaeb12 <ehabmalaeb2@gmail.com>
2025-12-18 03:06:59 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,41 @@
-const CACHE_NAME = "pricehunter-v1";
+const CACHE_NAME = 'uae-price-hunter-v2'; // Change this v2 to v3 next time you update
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json"
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+// Install Event - Clean up old caches immediately
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting(); // Force the new service worker to become active
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+// Activate Event - Delete old versions of the app from user's phone
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  return self.clients.claim(); // Take control of all open tabs immediately
 });
+
+// Fetch Event - Serve from cache but update in background
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
