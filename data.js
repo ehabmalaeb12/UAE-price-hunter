972b3e4ddacfb99c1ee5fbc353ae8a91adb70e3c
ehabmalaeb12 <ehabmalaeb2@gmail.com>
2025-12-24 14:44:39 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,26 @@
+// data.js
+// UAE Price Hunter - Demo Product Data
+
+window.SHOPPING_SOURCES = [
+  {
+    id: "iphone-15",
+    name: "Apple iPhone 15 128GB",
+    image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-15-blue-select-202309?wid=470&hei=556",
+    stores: [
+      { store: "Amazon UAE", price: 3399, link: "#" },
+      { store: "Noon", price: 3349, link: "#" },
+      { store: "Sharaf DG", price: 3449, link: "#" }
+    ]
+  },
+  {
+    id: "iphone-14",
+    name: "Apple iPhone 14 128GB",
+    image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-14-purple-select-202209?wid=470&hei=556",
+    stores: [
+      { store: "Amazon UAE", price: 2999, link: "#" },
+      { store: "Noon", price: 2949, link: "#" }
+    ]
+  }
+];
+
+console.log("✅ data.js loaded");
